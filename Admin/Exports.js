58ffc8be0fa6c10a174b34365567fe1049a27689
Saeed Nemati <sn.1361@gmail.com>
2022-06-25 16:51:47 +0430

    committed by script

---
+++ Admin/Exports.js
@@ -0,0 +1,16 @@
+import Transactions from './Transaction/List'
+
+const PaymentRoutes = [
+    {
+        path: "/transactions",
+        component: Transactions
+    }
+]
+
+const PaymentMenu = [
+    {
+
+    }
+]
+
+export { PaymentRoutes }
